0baf2d56f8a5a63beadec80b85217da988feea91
imskanand <the.anand3099@gmail.com>
2023-06-22 20:55:17 +0530

    Added Necessary details about Myself - added image - Text - Pressable drive link - ✅

--- App.tsx
+++ App.tsx
@@ -1,12 +1,31 @@
 import { StatusBar } from "expo-status-bar";
-import { Text, View, Pressable, Alert } from "react-native";
+import React from "react";
+import { Text, View, Pressable, Image, Linking } from "react-native";
+
+const driveUrl =
+  "https://drive.google.com/file/d/1ioYmY4Q2DmomA7HSvluHtQSUTdV7gi7t/view?usp=drive_link";
 
 export default function App() {
   return (
-    <View className="flex-1 items-center justify-center bg-black">
-      <Pressable onPress={() => Alert.alert("Welcome to the Portfolio!")}>
-        <Text className="font-bold text-xl button p-3 bg-teal-300 rounded-xl">
-          Portfolio
+    <View className="flex-1 items-center bg-stone-700 justify-center">
+      <Image
+        className="rounded-full w-48 h-48 my-5 border-3"
+        source={require("./assets/portfolio-image.png")}
+        alt="shubham image"
+        fadeDuration={300}
+        blurRadius={1}
+      />
+      <Text className="font-bold text-xl text-white rounded-xl text-center mx-20 my-5">
+        Hello, I'm Shubham, Full Stack Developer
+      </Text>
+      <Text className="font-normal text-base text-white mx-8 my-5 text-left">
+        Experienced Full Stack Developer proficient in Next.js, Tailwind CSS,
+        and TypeScript. Skilled in designing and developing efficient web
+        applications with a passion for problem-solving and continuous learning.
+      </Text>
+      <Pressable onPress={() => Linking.openURL(driveUrl)}>
+        <Text className="font-bold text-xl button px-3 bg-stone-200 rounded py-2 invert drop-shadow-xl blur-md">
+          Download Resume
         </Text>
       </Pressable>
       <StatusBar style="light" />
